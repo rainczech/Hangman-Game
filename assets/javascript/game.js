d5127a7929f19cef0f8f4709f5c09ea9614e2546
Grace Ho <graceho@Graces-MacBook-Pro.local>
2018-02-12 18:18:03 -0600

    framework for html

---
+++ assets/javascript/game.js
@@ -0,0 +1,26 @@
+
+// something to begin the game
+
+// Number of tries countdown
+var tries
+var showTries = document.getElementById("tries");
+// record guesses
+// var guess = 
+
+// stored guesses
+// var guesses = [];
+// guesses.push ("");
+// previous guesses won't affect countdown
+
+// words to guess
+
+var words=["south korea", "biathlon", "skis", "swimming"];
+
+var rand = words[Math.floor(Math.random() * words.length)];
+
+console.log(rand);
+
+
+
+
+
