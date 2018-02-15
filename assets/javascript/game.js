b7e5f43db451537c0e459d72f89c9902fb3c7597
Grace Ho <graceho@Graces-MacBook-Pro.local>
2018-02-14 18:08:12 -0600

    start game function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,26 +1,29 @@
-
-// something to begin the game
-
-// Number of tries countdown
-var tries
-var showTries = document.getElementById("tries");
-// record guesses
-// var guess = 
-
-// stored guesses
-// var guesses = [];
-// guesses.push ("");
-// previous guesses won't affect countdown
-
-// words to guess
-
-var words=["south korea", "biathlon", "skis", "swimming"];
-
-var rand = words[Math.floor(Math.random() * words.length)];
-
-console.log(rand);
-
-
-
-
-
+// variables
+// ===================
+var words=["korea", "biathlon", "skis", "snow"];
+var wins = 0;
+var loss = 0;
+var userGuesses = [];
+var wrongChoice = [ ];
+var triesLeft = 10;
+var spaces = [];
+// functions
+// ===================
+// pick a word at random
+function startGame(){
+    var choice = words[Math.floor(Math.random() * words.length)];
+    console.log(choice);
+    console.log(choice.length);
+// get the underscores
+    for(var i=0; i<choice.length; i++){
+        spaces.push("_");
+    }
+// print underscores to screen
+    document.getElementById("wordBlanks").textContent = spaces.join(" ");
+// reset
+userGuesses = [];
+triesLeft = 10; 
+}:
+// main
+// ===================
+startGame ()
