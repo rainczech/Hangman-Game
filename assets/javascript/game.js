5e002b3a2e2fea02a368c7cd52e4a17c264b8cfb
Grace Ho <graceho@graces-mbp.attlocal.net>
2018-02-15 21:51:37 -0600

    trying to get guesses to match word

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,26 +4,48 @@ var words=["korea", "biathlon", "skis", "snow"];
 var wins = 0;
 var loss = 0;
 var userGuesses = [];
-var wrongChoice = [ ];
+var wrongChoice = [];
 var triesLeft = 10;
 var spaces = [];
+var choice = words[Math.floor(Math.random() * words.length)]
 // functions
 // ===================
 // pick a word at random
 function startGame(){
     var choice = words[Math.floor(Math.random() * words.length)];
     console.log(choice);
-    console.log(choice.length);
 // get the underscores
     for(var i=0; i<choice.length; i++){
         spaces.push("_");
     }
 // print underscores to screen
     document.getElementById("wordBlanks").textContent = spaces.join(" ");
-// reset
+// values reset
 userGuesses = [];
-triesLeft = 10; 
-}:
+wrongChoice = [];
+triesLeft - 10;
+document.getElementById("tries-left").textContent = triesLeft;   
+};
+
+// log users keys/guesses
+document.onkeyup = function(event) {
+    userGuesses = event.key;
+    if(choice.indexOf(userGuesses) > -1) {
+        console.log("yes");
+    }
+    else {
+        wrongChoice.push(userGuesses);
+        console.log(wrongChoice);
+        triesLeft--;
+        console.log(triesLeft);
+    }
+
+}
+
+
+
+
+
 // main
 // ===================
 startGame ()
