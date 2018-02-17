7ac90365241f9068cb6d0c10dcc41701ee76eb24
Grace Ho <graceho@graces-mbp.attlocal.net>
2018-02-16 23:01:29 -0600

    added win function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,6 @@
 // variables
 // ===================
-var words=["korea", "biathlon", "skis", "snow"];
+var words=["korea", "bobsled", "skis", "snow"];
 var wins = 0;
 var loss = 0;
 var userGuesses = [];
@@ -8,7 +8,12 @@ var wrongChoice = [];
 var triesLeft = 10;
 var spaces = [];
 var choice = "";
-// var userGuesses = choice.fromCharCode(event.which).toLowerCase();
+var correctChoice = 0;
+var alphabetArray = ["a","b","c","d","e","f","g",
+                    "h","i","j","k","l","m","n",
+                    "o","p","q","r","s","t","u",
+                    "v","w","x","y","z"];
+// ==================
 // functions
 // ===================
 // pick a word at random
@@ -28,24 +33,41 @@ triesLeft = 10;
 document.getElementById("tries-left").textContent = triesLeft;   
 };
 
+function win(){
+    if(correctChoice === choice.length){
+        alert("You Won!");
+    } else if(triesLeft === 0){
+        alert("Sorry! You lose!");
+    }
+};
+
+
 // log users keys/guesses
 document.onkeyup = function() {
-    // userGuesses = event.key
     userGuesses = event.key.toLowerCase();
-    if(choice.indexOf(userGuesses) > -1) {
+    if(alphabetArray.indexOf(userGuesses) > -1) {
+        if(choice.indexOf(userGuesses)>-1){
         for(i=0; i<choice.length; i++) {
             if (choice[i] === userGuesses)
             {
                 spaces[i]= userGuesses;
+                document.getElementById("wordBlanks").textContent = spaces.join (" ");
                 console.log(spaces);
+                correctChoice++;
+                win();
             }
         }
-    }
-    else {
+    } else {
         wrongChoice.push(userGuesses);
         console.log(wrongChoice);
         triesLeft--;
         console.log(triesLeft);
+        document.getElementById("tries-left").textContent = triesLeft;
+        win();
+    }
+    } else{
+        alert("Letters Only!");
+
     }
 };
 
